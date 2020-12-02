f3e08f823b7bf2ee6a9b934aadb81d24d6d1dbe4
Maria Dubatto <mariadubatto@gmail.com>
2020-12-02 14:19:13 -0500

    morning update: Dec 02, 2020 14:19:13

--- src/nlp_pipeline.py
+++ src/nlp_pipeline.py
@@ -19,7 +19,7 @@ import gensim
 from sklearn.model_selection import train_test_split
 
 
-
+print('Hello')
 
 def extract_bow_from_raw_text(text_as_string):
     """Extracts bag-of-words from a raw text string.
@@ -39,8 +39,12 @@ def extract_bow_from_raw_text(text_as_string):
         return []
     text_as_string = text_as_string.lower()
     tokens = word_tokenize(text_as_string)
-
-    return tokens
+    # stopwords_ = set(stopwords.words('english'))
+    tokens_wo_stop = [w for w in tokens if w not in string.punctuation]
+    wordnet = WordNetLemmatizer()
+    tokens_lemm = list(map(wordnet.lemmatize, tokens_wo_stop))
+    
+    return tokens_lemm
 
 def vectorize(bow, model, size):
     """
@@ -54,12 +58,12 @@ def vectorize(bow, model, size):
                 row_vec += model.wv[word]
             except:
                 pass
-        corpus_vec[i] = row_vec
+        corpus_vec[i] = row_vec/len(row)
     return corpus_vec
 
-def word_embed(bow_train, bow_test, size=100, seed=2):
+def word_embed(bow_train, bow_test, min_count=2, size=100, seed=2):
 
-    model = gensim.models.Word2Vec(bow_train, min_count = 1, size = size, window = 5, seed=seed)
+    model = gensim.models.Word2Vec(bow_train, min_count = min_count, size = size, window = 5, seed=seed)
 
     vec_train = vectorize(bow_train, model, size)
     vec_test = vectorize(bow_test, model, size)
